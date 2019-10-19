0bc61128be2757dc7d15c1b9ff8f891e9f6e9646
贺世超 <463494980@qq.com>
2019-10-19 21:42:52 +0800

    mytest  更新

--- models/my_Thread.py
+++ models/my_Thread.py
@@ -1,1 +1,34 @@
+import threading
+import time
 
+#定义线程要执行的函数
+def writing():
+    # a = 0
+    # c = 1
+    # while a <= 4:
+    #     print("我写的第%s篇字" % c)
+    #     c += 1
+    #     a += 1
+    print("我在写字的哦")
+    time.sleep(2)     #沉睡2秒，方便我们看到执行的过程
+
+#定义线程要执行的函数
+def drawing():
+    print("我在画画的哦")
+    time.sleep(2)     #沉睡2秒，方便我们看到执行的过程
+
+def main():
+    t1 = threading.Thread(target=writing)     #创建线程1
+    t2 = threading.Thread(target=drawing)     #创建线程2
+    t1.start()
+    t2.start()
+    time.sleep(1)    #沉睡1秒，方便我们看到执行的过程
+#
+# def main():
+#     writing()
+#     drawing()
+
+if __name__ == '__main__':
+    main()
+    main()
+    main()
